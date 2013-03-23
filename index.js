9447c493566a47a7b5a9d4c8604655bed0eeb69a
Mikito Takada <mikito.takada@gmail.com>
2013-03-22 19:48:19 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// EDB facts
+var edb = {
+  parent: [
+    [ constant('bill'), constant('mary') ],
+    [ constant('mary'), constant('john') ]
+  ]
+};
+
+// IDB rules
+var idb = {
+  ancestor: [
+    {
+      [ vari('X'), vari('Y') ], // :-
+      { parent: [ vari('X'), vari('Y') ] }
+    },
+    {
+      [ vari('X'), vari('Y') ], // :-
+      { parent: [ vari('X'), vari('Z') ] },
+      { ancestor: [ vari('Z'), vari('Y') ] }
+    }
+  ]
+};
+
+var goal = { ancestor: [ 'bill', undefined ] };
+
+/*
+
+  ancestor('bill', undefined)
+
+  Should generate:
+
+  ancestor('bill', 'mary')
+  ancestor('bill', 'john')
+
+  By doing something like:
+
+  1. ancestor('bill', 'mary') <= parent('bill', 'mary')
+  2. ancestor('bill', 'john') <= parent('bill', 'mary')
+                               & ancestor('mary', 'john')
+                               <= parent('mary', 'john')
+
+
+*/
